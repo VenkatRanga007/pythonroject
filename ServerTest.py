d25b98f847439817ce8293591587b838bf90fc67
VenkatRanga007 <venkat.ranga.k@gmail.com>
2022-04-14 18:55:43 +0530

    updated

--- ServerTest.py
+++ ServerTest.py
@@ -1,26 +1,33 @@
 import paramiko, glob
+from stat import S_ISDIR
 
 host="3.109.121.57"
 username="Administrator"
 password="Up3HNKGL-22f3ES-u%Fz)YJUMNcx%uDK"
-
-#host="172.31.14.87"
-#username="Administrator"
-#password="DMWA76YU2WpYK&g@9yd*is7d4!Ps((WN"
 
 client = paramiko.SSHClient()
 client.load_system_host_keys()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(host, 22, username=username, password=password)
 print("connected")
+sftp = client.open_sftp()
 
-try:
-    stdin, stdout, stderr = client.exec_command('dir')
-    out = ''.join(stdout.readlines())
-    print(out)
-except Exception as e:
-    print(e.message)
-    err = ''.join(stderr.readlines())
-    print(err)
+def sftpwalk(dl):
+    remotepath = dl
+    files = []
+    folders = []
+    for f in sftp.listdir_attr(remotepath):
+        if S_ISDIR(f.st_mode):
+            folders.append(f.filename)
+            # print(dl+f.filename)
+            sftpwalk(dl + f.filename + "/")
+        else:
+            # if
+            files.append(f.filename)
+    # print (path,folders,files)
+    print(remotepath, folders, files)
 
-client.close()
+
+dl = 'C://Users/Administrator/Desktop/'
+sftpwalk(dl)
+client.close
